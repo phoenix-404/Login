cbc0ecbfbec9c97ee5b9eca105ff53ec9c00c137
Sourav Karmakar <sourav.official321@gmail.com>
2025-04-02 19:01:52 +0530

    commit

--- backend/login-signup-springboot-security-thymeleaf-mysql/src/test/java/com/example/login_signup_springboot_security_thymeleaf_mysql/LoginSignupSpringbootSecurityThymeleafMysqlApplicationTests.java
+++ backend/login-signup-springboot-security-thymeleaf-mysql/src/test/java/com/example/login_signup_springboot_security_thymeleaf_mysql/LoginSignupSpringbootSecurityThymeleafMysqlApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.login_signup_springboot_security_thymeleaf_mysql;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
